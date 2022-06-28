8c95d7a09457ec05cf73c96aedd1e2599fc27d3b
hdenguir <hafid@reezocar.com>
2022-06-28 10:34:47 +0200

    jest config

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -5,87 +5,87 @@ import { checkoutCart, getTotalPrice, removeFromCart, updateQuantity } from '../
 import styles from './Cart.module.css';
 
 export function Cart() {
-   const items = useAppSelector((state) => state.cart.items);
-   const products = useAppSelector((state) => state.products.products);
-   const totalPrice = useAppSelector(getTotalPrice);
-   const dispatch = useAppDispatch();
-   const checkoutState = useAppSelector((state) => state.cart.checkoutState);
-   const errorMessage = useAppSelector((state) => state.cart.errorMessage);
+  const items = useAppSelector((state) => state.cart.items);
+  const products = useAppSelector((state) => state.products.products);
+  const totalPrice = useAppSelector(getTotalPrice);
+  const dispatch = useAppDispatch();
+  const checkoutState = useAppSelector((state) => state.cart.checkoutState);
+  const errorMessage = useAppSelector((state) => state.cart.errorMessage);
 
-   const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
-      e.preventDefault();
-      if (e.target.value) {
-         const quantity = Number(e.target.value);
+  const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    e.preventDefault();
+    if (e.target.value) {
+      const quantity = Number(e.target.value);
 
-         dispatch(updateQuantity({ id, quantity }));
-      }
-   };
+      dispatch(updateQuantity({ id, quantity }));
+    }
+  };
 
-   const onCheckout = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+  const onCheckout = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-      dispatch(checkoutCart());
-   };
+    dispatch(checkoutCart());
+  };
 
-   const tableClasses = classNames({
-      [styles.table]: true,
-      [styles.checkoutError]: checkoutState === 'ERROR',
-      [styles.checkoutLoading]: checkoutState === 'LOADING',
-   });
+  const tableClasses = classNames({
+    [styles.table]: true,
+    [styles.checkoutError]: checkoutState === 'ERROR',
+    [styles.checkoutLoading]: checkoutState === 'LOADING',
+  });
 
-   return (
-      <main className="page">
-         <h1>Shopping Cart</h1>
-         <table className={tableClasses}>
-            <thead>
-               <tr>
-                  <th>Product</th>
-                  <th>Quantity</th>
-                  <th>Total</th>
-                  <th>Remove</th>
-               </tr>
-            </thead>
-            <tbody>
-               {Object.entries(items).map(([id, quantity]) => (
-                  <tr key={id}>
-                     <td>{products[id].name}</td>
-                     <td>
-                        <input
-                           type="text"
-                           className={styles.input}
-                           defaultValue={quantity}
-                           onChange={(e) => onQuantityChange(e, id)}
-                        />
-                     </td>
-                     <td>{products[id].price}</td>
-                     <td>
-                        <button
-                           aria-label={`Remove ${products[id].name} from Shopping Cart`}
-                           onClick={() => dispatch(removeFromCart(id))}
-                        >
-                           X
-                        </button>
-                     </td>
-                  </tr>
-               ))}
-            </tbody>
-            <tfoot>
-               <tr>
-                  <td>Total</td>
-                  <td></td>
-                  <td className={styles.total}>${totalPrice}</td>
-                  <td></td>
-               </tr>
-            </tfoot>
-         </table>
-         <form onSubmit={onCheckout}>
-            {checkoutState === 'ERROR' && errorMessage ? (
-               <p className={styles.errorBox}>{errorMessage}</p>
-            ) : null}
-            <button className={styles.button} type="submit">
-               Checkout
-            </button>
-         </form>
-      </main>
-   );
+  return (
+    <main className="page">
+      <h1>Shopping Cart</h1>
+      <table className={tableClasses}>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Total</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(items).map(([id, quantity]) => (
+            <tr key={id}>
+              <td>{products[id].name}</td>
+              <td>
+                <input
+                  type="text"
+                  className={styles.input}
+                  defaultValue={quantity}
+                  onChange={(e) => onQuantityChange(e, id)}
+                />
+              </td>
+              <td>{products[id].price}</td>
+              <td>
+                <button
+                  aria-label={`Remove ${products[id].name} from Shopping Cart`}
+                  onClick={() => dispatch(removeFromCart(id))}
+                >
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Total</td>
+            <td></td>
+            <td className={styles.total}>${totalPrice}</td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
+      <form onSubmit={onCheckout}>
+        {checkoutState === 'ERROR' && errorMessage ? (
+          <p className={styles.errorBox}>{errorMessage}</p>
+        ) : null}
+        <button className={styles.button} type="submit">
+          Checkout
+        </button>
+      </form>
+    </main>
+  );
 }
